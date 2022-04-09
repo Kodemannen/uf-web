a5fda0a03ed3cdee0181726e33872f3f88c01be9
kodemannen <samuelknudsen@hotmail.com>
2022-04-09 14:39:49 +0200

    Added fade to video

--- cut_video.py
+++ cut_video.py
@@ -3,20 +3,22 @@ import moviepy as mp
 import moviepy.editor as mpe
 import os
 
-path = "src/images/Video/sample\ footage.mp4"
-# path = 'src/images/Video/sample\ footage.mp4'
-path = 'src/images/Video/sample footage.mp4' 
-path = 'src/videos/sample footage.mp4' 
-path = os.path.expanduser("~/Videos/urban-farms/sample footage.mp4")
+# original:
+pathOriginal = os.path.expanduser("~/Videos/urban-farms/sample footage.mp4")
 
-# Load myHolidays.mp4 and select the subclip 00:00:50 - 00:00:60
 
-# Load select the subclip 00:00:50 - 00:00:60
-start = 60*3 + 3
-end = start + 24
-clip = mpe.VideoFileClip(path).subclip(start,end)
+# Load a supclip from the video
+start = 60*3 + 2.85
+end = start + 7.0
+clip = mpe.VideoFileClip(pathOriginal).subclip(start,end)
 
-clip.write_videofile("src/videos/vid.mp4")
-# with open(path, "r") as f:
-#     print("hmm")
+# clip.fadeout(2,final_color="black")
+
+# mp.video.fx.all.fadeout(clip, duration=2, final_color=[0,0,0])
+# clip.fadeout(duration=2, final_color=[0,0,0])
+clip = clip.fadein(duration=1, initial_color=[0,0,0])
+clip = clip.fadeout(duration=1, final_color=[0,0,0])
+# clip.fadein(duration=2, 0)
+
+clip.write_videofile("src/media/videos/vid.mp4")
 
