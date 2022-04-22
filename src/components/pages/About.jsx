4d560dd15fff3053edfefad3589fb4b60e58fdfb
kodemannen <samuelknudsen@hotmail.com>
2022-04-22 11:00:52 +0200

    Begynner å bli nice

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -15,7 +15,8 @@ function About() {
   return (
     <div className="page">
 
-      <Card className="card" body outline >
+      <Card outline className="cardBody" >
+
         <CardBody >
           <CardTitle style={{textAlign: "center"}} tag="h5">
           </CardTitle>
@@ -24,10 +25,20 @@ function About() {
             tag="h6"
           >
           </CardSubtitle>
-          <CardText>
-            We are a group of 
+
+          <CardText style={{color: "white", opacity: "0.65"}}>
+            We are a collection of STEM oriented individuals who want
+            to help improving the future of local food security.
           </CardText>
         </CardBody>
+        <image 
+          style={{
+            opacity: "0.65", 
+          }}
+          src={require('./../../media/images/trestk.jpg')} 
+          type="image/jpg"
+        />
+
       </Card>
       
     </div>
